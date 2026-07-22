9bbaf378e4a1754eca1daa0807d66dd524b95f84
giabao1411 <giabao819@gmail.com>
2026-07-22 09:37:23 +0700

    update sqlmedium ex

--- SQLMedium.sql
+++ SQLMedium.sql
@@ -58,3 +58,76 @@ select id, age ,coins_needed, power
 from RankedWands
 where rnk = 1 
 order by power desc , age desc
+-- Câu 5: Challenges
+select  h.hacker_id , h.name , COUNT(c.challenge_id) 
+from hackers as h join challenges as c on h.hacker_id = c.hacker_id 
+group by h.hacker_id ,h.name
+having 
+COUNT(c.challenge_id) = (SELECT TOP 1 COUNT(c.challenge_id) from  challenges as c
+group by c.hacker_id
+order by COUNT(c.challenge_id) desc ) 
+OR COUNT(c.challenge_id) IN (
+    SELECT cnt 
+    from (select count(challenge_id) as cnt from challenges group by hacker_id) as temp 
+    group by cnt
+    having count(cnt)=1
+)
+order by COUNT(c.challenge_id) desc ,h.hacker_id
+-- Câu 6: Contest Leaderboard
+WITH MaxScore as( 
+    SELECT hacker_id,challenge_id ,MAX(score) as maxscore from submissions group by challenge_id,hacker_id
+)
+
+select h.hacker_id, h.name , SUM(s.maxscore)
+from hackers as h join MaxScore as s on h.hacker_id = s.hacker_id
+group by h.hacker_id ,h.name
+having sum(s.maxscore)>0
+order by sum(s.maxscore) desc , h.hacker_id asc 
+-- Câu 7: SQL Project Planning (Gấp dải thời gian (Gaps and Islands Problem))
+--StarDate không nằm trong End_Date nào thì là ngày bắt đầu dự án
+WITH Starters AS (
+    
+    SELECT Start_Date, ROW_NUMBER() OVER (ORDER BY Start_Date) AS rn
+    FROM Projects
+    WHERE Start_Date NOT IN (SELECT DISTINCT End_Date FROM Projects)
+),
+--EndDate không nằm trong StarDate nào thì là ngày kết thúc dự án 
+Enders AS (
+    
+    SELECT End_Date, ROW_NUMBER() OVER (ORDER BY End_Date) AS rn
+    FROM Projects
+    WHERE End_Date NOT IN (SELECT DISTINCT Start_Date FROM Projects)
+)
+--Sau khi sắp xếp đánh thứ tự thì nối theo row thì ta có 1 bộ dữ liệu liền kề
+SELECT 
+    s.Start_Date, 
+    e.End_Date
+FROM Starters s
+JOIN Enders e ON s.rn = e.rn
+ORDER BY 
+    DATEDIFF(day, s.Start_Date, e.End_Date) ASC, 
+    s.Start_Date ASC;
+-- Câu 8: Placements
+select s.name 
+from students as s join friends as f on s.id= f.id
+-- Lấy lương của sv đó
+ join packages as p_stu on s.id = p_stu.id 
+ -- lấy lương của bạn thân sv đó
+ join packages as p_fri on f.friend_id=p_fri.id
+ where p_stu.salary < p_fri.salary
+ order by p_fri.salary 
+ -- Câu 9: 
+ SELECT f1.X, f1.Y
+FROM Functions AS f1
+JOIN Functions AS f2 
+    ON f1.X = f2.Y 
+   AND f1.Y = f2.X
+GROUP BY f1.X, f1.Y
+HAVING 
+    -- Trường hợp 1: X < Y (Luôn đúng nếu tìm thấy cặp khớp)
+    f1.X < f1.Y 
+    
+    -- Trường hợp 2: X = Y (Bắt buộc phải xuất hiện từ 2 dòng trở lên)
+    OR (f1.X = f1.Y AND COUNT(*) > 1)
+
+ORDER BY f1.X ASC;
