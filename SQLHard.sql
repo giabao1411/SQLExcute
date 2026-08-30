7b5cbb9b953c1c81ace3801d91720112ac10aa20
giabao1411 <giabao819@gmail.com>
2026-08-30 08:51:29 +0700

    update hard

--- SQLHard.sql
+++ SQLHard.sql
@@ -644,3 +644,14 @@ WHERE prev_active_month IS NULL
    OR active_month > prev_active_month + INTERVAL '1 month'
 GROUP BY active_month
 ORDER BY mth;
+--Câu 22 : Senior Managers
+SELECT 
+  managers.manager_name,
+  COUNT(DISTINCT managers.emp_id) AS direct_reportees
+FROM employees
+JOIN employees AS managers
+  ON employees.manager_id = managers.emp_id
+JOIN employees AS senior_managers
+  ON managers.manager_id = senior_managers.emp_id
+GROUP BY managers.manager_name
+ORDER BY direct_reportees DESC;
